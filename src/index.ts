d94640d288199abf45411885281d1097724679e6
Adam Hingoro <adam.abdulshakoor@gmail.com>
2020-04-08 13:25:30 +0500

    initial index file

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express  from "express";
+// const express = require( "express" );
+const app = express();
+const port = 8080; // default port to listen
+
+
+// define a route handler for the default home page
+app.get( "/", (   req : express.Request ,   res : express.Response ) => {
+    res.send( "Hello world!" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
